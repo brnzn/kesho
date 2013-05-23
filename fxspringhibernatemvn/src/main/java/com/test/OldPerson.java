65abcbed520bf23eb341dd88e042fc8412662c92
oren <oren.berenson@gmail.com>
2013-05-23 23:11:54 +0100

    fx poc

---
+++ fxspringhibernatemvn/src/main/java/com/test/OldPerson.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class OldPerson
+{
+    private String firstName;
+ 
+    public OldPerson(String firstName)
+    {
+        this.firstName = firstName;
+    }
+ 
+    public String getFirstName()
+    {
+        return firstName;
+    }
+}
