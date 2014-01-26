09613486fe11ccab695b4fa5f13afe11a9a43afd
oren <oren.berenson@gmail.com>
2014-01-26 23:31:45 +0000

    family

--- project/ui/src/main/java/com/kesho/datamart/ui/controller/FamilyDialogController.java
+++ project/ui/src/main/java/com/kesho/datamart/ui/controller/FamilyDialogController.java
@@ -1,13 +1,14 @@
 package com.kesho.datamart.ui.controller;
 
+import com.kesho.datamart.domain.Location;
 import com.kesho.datamart.dto.EducationDto;
 import com.kesho.datamart.dto.FamilyDto;
 import com.kesho.datamart.ui.WindowsUtil;
 import com.kesho.datamart.ui.repository.FamilyRepository;
 import com.kesho.datamart.ui.util.Event;
+import com.kesho.datamart.ui.util.Util;
 import javafx.fxml.FXML;
-import javafx.scene.control.Dialogs;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import org.apache.commons.lang.StringUtils;
 
@@ -20,11 +21,28 @@ import javax.inject.Named;
 public class FamilyDialogController {
     @Inject
     private FamilyRepository repository;
+    private Long id;
 
     @FXML
-    private TextField family;
+    private TextField familyName;
+    private ComboBox<Location> homeLocation;
+    private TextField homeSubLocation;
+    private TextField homeClusterId;
+    private Integer aliveParents;
+    private ToggleGroup isMarried;
+    private TextField numNonKeshoStudents;
+    private TextField numOfWives;
+    private TextField primaryCaretaker;
+    private TextField mainContactName;
+    private TextField mobileNumber;
+    private ToggleGroup isPhoneOwner;
+    private TextField phoneOwnerName;
+    private TextField numOfStudentsAtAddress;
+    private TextArea profile;
+    private TextField numOfAdultsAtAddress;
 
-	private Stage dialogStage;
+
+    private Stage dialogStage;
 	private EducationDto dto;
 	/**
 	 * Initializes the controller class. This method is automatically called
@@ -32,6 +50,7 @@ public class FamilyDialogController {
 	 */
 	@FXML
 	private void initialize() {
+        Util.initializeYesNoGroup(isMarried, isPhoneOwner);
 	}
 	
 	/**
@@ -48,7 +67,7 @@ public class FamilyDialogController {
 	@FXML
 	private void handleOk() {
         if (isInputValid()) {
-            FamilyDto family = new FamilyDto(null, this.family.getText());
+            FamilyDto family = new FamilyDto(null, this.familyName.getText());
             repository.save(family);
             WindowsUtil.getInstance().getEventBus().fireEvent(Event.FAMILY_ADDED);
 			dialogStage.close();
@@ -71,7 +90,7 @@ public class FamilyDialogController {
 	private boolean isInputValid() {
 		String errorMessage = "";
 
-		if (StringUtils.isBlank(family.getText())) {
+		if (StringUtils.isBlank(familyName.getText())) {
 			errorMessage += "Institution Name!\n";
 		}
 
@@ -84,4 +103,6 @@ public class FamilyDialogController {
 			return false;
 		}
 	}
+
+
 }
