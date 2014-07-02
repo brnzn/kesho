2b5e2fc05cf1da021c9a2ae4157a4232687b9361
oren <oren.berenson@gmail.com>
2014-07-02 07:09:01 +0100

    changes based on kate comments

---
+++ project/datamart/src/main/java/com/kesho/datamart/dto/Dto.java
@@ -0,0 +1,12 @@
+package com.kesho.datamart.dto;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: orenberenson
+ * Date: 6/30/14
+ * Time: 5:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Dto {
+    Long getId();
+}
